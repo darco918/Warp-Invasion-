130bc66e72d43da7c7ecac702797fc9e5843b7f5
Darco Murja <darcomurja@gmail.com>
2020-11-22 03:14:49 +0000

    opening activity and some gradients

---
+++ app/src/main/java/com/example/warpinvasion30/StartActivity.kt
@@ -0,0 +1,17 @@
+package com.example.warpinvasion30
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_start.*
+
+class StartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+
+        loginButton.setOnClickListener(){
+            startActivity(Intent(this, LogInActivity::class.java))
+        }
+    }
+}
