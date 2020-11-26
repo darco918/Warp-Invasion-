62f51f06675e139537978b1afc4fb45318efef1e
Darco Murja <darcomurja@gmail.com>
2020-11-26 10:25:57 +0000

    Implemented the recycleview in the fragment_weekly_challenges.xml. It works perfectly, needs more design details

--- app/src/main/java/com/example/warpinvasion30/RecyclerviewTestActivity.kt
+++ app/src/main/java/com/example/warpinvasion30/RecyclerviewTestActivity.kt
@@ -23,41 +23,41 @@ class RecyclerviewTestActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recyclerview_test)
 
-        manager = LinearLayoutManager(this)
-        myAdapter = MyAdapter(values)
-
-        recyclerView = findViewById<RecyclerView>(R.id.recycler_view).apply {
-            layoutManager = manager
-            adapter =  myAdapter
-        }
+//        manager = LinearLayoutManager(this)
+//        myAdapter = MyAdapter(values)
+//
+//        recyclerView = findViewById<RecyclerView>(R.id.recycler_view).apply {
+//            layoutManager = manager
+//            adapter =  myAdapter
+    //    }
     }
 }
-
- class MyAdapter(private val myDataSet : Array<String>):
-    RecyclerView.Adapter<MyAdapter.ViewHolder>() {
-
-    class ViewHolder(val view: View): RecyclerView.ViewHolder(view){
-        fun bind(text: String){
-            val tv = view.findViewById<TextView>(R.id.textView)
-            tv.text = text
-
-            val j= 66
-            val progressBarChallenge = view.findViewById<ProgressBar>(R.id.progressBarChallenge)
-            progressBarChallenge.progress = j
-        }
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val vh = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent,false)
-        return ViewHolder(vh)
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(myDataSet[position])
-    }
-
-    override fun getItemCount(): Int {
-        return myDataSet.size
-    }
-
-}
+//
+// class MyAdapter(private val myDataSet : Array<String>):
+//    RecyclerView.Adapter<MyAdapter.ViewHolder>() {
+//
+//    class ViewHolder(val view: View): RecyclerView.ViewHolder(view){
+//        fun bind(text: String){
+//            val tv = view.findViewById<TextView>(R.id.textView)
+//            tv.text = text
+//
+//            val j= 66
+//            val progressBarChallenge = view.findViewById<ProgressBar>(R.id.progressBarChallenge)
+//            progressBarChallenge.progress = j
+//        }
+//    }
+//
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+//        val vh = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent,false)
+//        return ViewHolder(vh)
+//    }
+//
+//    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+//        holder.bind(myDataSet[position])
+//    }
+//
+//    override fun getItemCount(): Int {
+//        return myDataSet.size
+//    }
+//
+//}
--- app/src/main/java/com/example/warpinvasion30/WeeklyChallengesFragment.kt
+++ app/src/main/java/com/example/warpinvasion30/WeeklyChallengesFragment.kt
@@ -9,11 +9,19 @@ import android.view.ViewGroup
 import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.fragment_profile.settingsButton
 import kotlinx.android.synthetic.main.fragment_weekly_challenges.*
 
 
 class WeeklyChallengesFragment : Fragment(){
+
+    val  values = arrayOf("one", "two" , "three", "four", "five" , "six", "seven", "eight", "nine", "ten")
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var manager: RecyclerView.LayoutManager
+    private lateinit var myAdapter: RecyclerView.Adapter<*>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,6 +29,9 @@ class WeeklyChallengesFragment : Fragment(){
     ): View? {
 
         return inflater.inflate(R.layout.fragment_weekly_challenges, container, false)
+
+
+
     }
 
     companion object {
@@ -40,14 +51,42 @@ class WeeklyChallengesFragment : Fragment(){
             startActivity(intent)
         }
 
-
-        val j = 100
-        val progressText = j.toString() + " %"
-        progressBarText.text = progressText
-        progressBar.progress = j
-
-
-        progressBarChallenge.progress = 50
+        manager = LinearLayoutManager(activity)
+        myAdapter = MyAdapter(values)
+        recyclerView =view.findViewById<RecyclerView>(R.id.recycler_view).apply {
+            layoutManager = manager
+            adapter =  myAdapter
+        }
 
     }
+}
+
+
+class MyAdapter(private val myDataSet : Array<String>):
+    RecyclerView.Adapter<MyAdapter.ViewHolder>() {
+
+    class ViewHolder(val view: View): RecyclerView.ViewHolder(view){
+        fun bind(text: String){
+            val tv = view.findViewById<TextView>(R.id.textView)
+            tv.text = text
+
+            val j= 66
+            val progressBarChallenge = view.findViewById<ProgressBar>(R.id.progressBarChallenge)
+            progressBarChallenge.progress = j
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val vh = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent,false)
+        return ViewHolder(vh)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(myDataSet[position])
+    }
+
+    override fun getItemCount(): Int {
+        return myDataSet.size
+    }
+
 }
