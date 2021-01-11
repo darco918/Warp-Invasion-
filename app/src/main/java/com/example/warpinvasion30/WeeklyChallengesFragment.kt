ab661142f940c2f8762a24f6961ca4c6c1f6ac11
Darco Murja <darcomurja@gmail.com>
2021-01-11 01:22:30 +0000

    second delivery

--- app/src/main/java/com/example/warpinvasion30/WeeklyChallengesFragment.kt
+++ app/src/main/java/com/example/warpinvasion30/WeeklyChallengesFragment.kt
@@ -14,11 +14,11 @@ import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.fragment_profile.settingsButton
 import kotlinx.android.synthetic.main.fragment_weekly_challenges.*
 
-
 class WeeklyChallengesFragment : Fragment(){
 
-    val  values = arrayOf("Defeat 30 mummies", "Get a double kill" , "Shoot enemies from above", "Survive a one minute without shooting", "Reach wave no. 10" )
-
+    val  values = arrayOf("Defeat mummies", "Get a double kill" , "Shoot enemies from above", "Survive a one minute without shooting", "Reach wave no. 10" )
+    val numbers = arrayOf(30,1,10,1,1)
+    val completed = arrayOf(21,1,5,0,1)
     private lateinit var recyclerView: RecyclerView
     private lateinit var manager: RecyclerView.LayoutManager
     private lateinit var myAdapter: RecyclerView.Adapter<*>
@@ -27,14 +27,11 @@ class WeeklyChallengesFragment : Fragment(){
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         return inflater.inflate(R.layout.fragment_weekly_challenges, container, false)
-
     }
 
     companion object {
         fun newInstance(): WeeklyChallengesFragment {
-
             return WeeklyChallengesFragment()
         }
 
@@ -44,13 +41,23 @@ class WeeklyChallengesFragment : Fragment(){
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        //checking how many challenges are complete and then updating the progressbar
+        var contor =0
+        for (i in numbers.indices){
+            if(numbers[i] == completed[i])
+                contor++
+        }
+        progressBar.progress = (contor / numbers.size.toDouble() * 100).toInt()
+        progressBarText.text = (contor / numbers.size.toDouble() * 100).toInt() . toString() + "%"
+
+
         settingsButton.setOnClickListener {
             val intent = Intent (activity, SettingsActivity::class.java)
             startActivity(intent)
         }
 
         manager = LinearLayoutManager(activity)
-        myAdapter = MyAdapter(values)
+        myAdapter = MyAdapter(values, numbers, completed)
         recyclerView =view.findViewById<RecyclerView>(R.id.recycler_view).apply {
             layoutManager = manager
             adapter =  myAdapter
@@ -60,15 +67,18 @@ class WeeklyChallengesFragment : Fragment(){
 }
 
 
-class MyAdapter(private val myDataSet : Array<String>):
+class MyAdapter(private val myDataSet : Array<String>, private  val numbersToComplete: Array<Int>,  private  val completed: Array<Int>):
     RecyclerView.Adapter<MyAdapter.ViewHolder>() {
 
     class ViewHolder(val view: View): RecyclerView.ViewHolder(view){
-        fun bind(text: String){
+        fun bind(text: String, number:Int, completed:Int){
             val tv = view.findViewById<TextView>(R.id.textView)
             tv.text = text
+            val numberOfCompletedChallenges = view.findViewById<TextView>(R.id.numberOfCompletedChallenges)
+            val aux = completed.toString()  +" /"+number.toString()
+            numberOfCompletedChallenges.text = aux
 
-            val j= 66
+            val j= (completed/number.toDouble() *100).toInt()
             val progressBarChallenge = view.findViewById<ProgressBar>(R.id.progressBarChallenge)
             progressBarChallenge.progress = j
         }
@@ -80,7 +90,7 @@ class MyAdapter(private val myDataSet : Array<String>):
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(myDataSet[position])
+        holder.bind(myDataSet[position], numbersToComplete[position],completed[position] )
     }
 
     override fun getItemCount(): Int {
